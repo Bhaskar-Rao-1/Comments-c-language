9826360687941dffb818e6380f02568022f5b23b
Bhaskar-Rao-1 <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-07-28 19:59:05 +0530

    Add files via upload

---
+++ comments.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    //single line comment
+    printf(" we use // for single line comment\n");
+    
+    /* multiple lines comment */
+    printf(" we use /*-------*/for multiple lines comment\n");
+    return 0;
+}
